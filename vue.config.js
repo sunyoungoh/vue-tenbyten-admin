ca755c5e1c79e7fd74eb839ac3e1d698f52cf02d
syeong <tjsdud7379@gmail.com>
2022-11-24 04:53:56 +0900

    edit publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/tenbyten-admin/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/vue-tenbyten-admin/' : '/',
   devServer: {
     proxy: process.env.VUE_APP_TENBYTEN_API_URL,
   },
