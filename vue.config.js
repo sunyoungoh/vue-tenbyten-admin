7c3bb5a633a7d70a27b6b60fb82fd5362eea94ea
sunyoungoh <tjsdud7379@gmail.com>
2022-12-12 21:48:23 +0900

    Delete dev proxy, .env

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,4 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production' ? '/vue-tenbyten-admin/' : '/',
-  // 개발 서버 설정
-  devServer: {
-    // 프록시 설정
-    proxy: {
-      '/api': {
-        target: process.env.VUE_APP_TENBYTEN_API_URL,
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' },
-      },
-      '/mail': {
-        target: process.env.VUE_APP_MAIL_API_URL,
-        changeOrigin: true,
-      },
-    },
-  },
 };
