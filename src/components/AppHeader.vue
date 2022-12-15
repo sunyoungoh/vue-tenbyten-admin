098d229a8970f20bc096c35cc50e913d9b49f138
sunyoungoh <tjsdud7379@gmail.com>
2022-12-16 07:05:39 +0900

    Update logo img

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div class="logo"><img src="@/assets/logo.png" alt="logo" /></div>
+    <div class="logout" v-if="isLogin" @click="logout">
+      <i class="uil uil-sign-out-alt"></i>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  computed: {
+    isLogin() {
+      return this.$store.state.user.isLogin;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout');
+    },
+  },
+};
+</script>
+
+<style></style>
