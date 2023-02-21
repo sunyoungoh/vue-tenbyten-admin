11e372fca63c78da8ec43058c12023390a7d5eda
sunyoungoh <tjsdud7379@gmail.com>
2023-02-21 12:29:08 +0900

    Add item id case

--- src/utils/getItemName.js
+++ src/utils/getItemName.js
@@ -37,6 +37,8 @@ export const getItemName = itemId => {
       return '교환일기';
     case 5161943:
       return '네컷 프레임 스티커';
+    case 5234189:
+      return '디데이,타임스탬프 스티커';
     default:
       return '';
   }
